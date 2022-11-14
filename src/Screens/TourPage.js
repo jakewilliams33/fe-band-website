9ef111e2fa0447aa150f8d77c8b88f388c2f92d4
jakewilliams33 <jakewilliams33@hotmail.com>
2022-11-14 01:20:58 +0000

    fix tour page transtition

--- src/Screens/TourPage.js
+++ src/Screens/TourPage.js
@@ -3,14 +3,14 @@ import { useScript } from "../Hooks/useScript";
 import "../Styles/TourPageStyles.css";
 import { motion } from "framer-motion";
 
+const transitions = {
+  initial: { width: 0, opacity: 0 },
+  animate: { width: "100%", opacity: 1 },
+  exit: { x: 5000, opacity: 0 },
+};
+
 export const TourPage = () => {
   useScript("//widget.songkick.com/10172348/widget.js");
-
-  const transitions = {
-    initial: { width: 0 },
-    animate: { width: "100%" },
-    exit: { x: 5000, opacity: 0 },
-  };
 
   return (
     <motion.div
@@ -24,7 +24,6 @@ export const TourPage = () => {
       <section className="skew-background-tour">
         <div className="widgetContainer">
           <h1>Tour</h1>
-
           <a
             href="https://www.songkick.com/artists/10172348"
             className="songkick-widget"
